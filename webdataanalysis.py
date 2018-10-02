4348836ee6c79ccc56e69812d979b27be964bf30
adityasinghal2 <singhal.aditya760@gmail.com>
2018-10-03 00:14:13 +0530

    code cleanup

--- webdataanalysis.py
+++ webdataanalysis.py
@@ -20,13 +20,11 @@ def parse_comment_page(page_url):
     extracted_comments = []
     for comment in comments: 
         if comment.find('form'):
-            #We are now looking for any element with a class of author in the comment, instead of just looking for a tags. 
-            #We noticed some comments whose authors have deleted their account shows up with a span tag instead of an a 
             commenter = comment.find(attrs={'class':'author'}).text
             comment_text = comment.find('div',attrs={'class':'md'}).text.strip()
             permalink = comment.find('a',attrs={'class':'bylink'})['href']
             extracted_comments.append({'commenter':commenter,'comment_text':comment_text,'permalink':permalink})
-    #Lets put the data in dict 
+
     post_data = {
         'title':title,
         'no_of_upvotes':upvotes,
@@ -36,10 +34,7 @@ def parse_comment_page(page_url):
     }
     return post_data
 try:
-    # url = 'https://www.google.com/search?q=python'
 
-    # now, with the below headers, we defined ourselves as a simpleton who is
-    # still using internet explorer.
     url = "https://old.reddit.com/top/"
     headers = {}
     headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
@@ -58,27 +53,20 @@ try:
     # print (soup.prettify())
     #get the HTML of the table called site Table where all the links are displayed
     main_table = soup.find("div",attrs={'id':'siteTable'})
-    #Now we go into main_table and get every a element in it which has a class "title" 
+
     comment_a_tags = main_table.find_all('a',attrs={'class':'bylink comments may-blank'})
-    #from each link extract the text of link and the link itself
-    #List to store a dict of the data we extracted 
-    #from each link extract the text of link and the link itself
-    #List to store a dict of the data we extracted 
+
     extracted_records = []
     for link in comment_a_tags: 
         title = link.text
         url = link['href']
-        #There are better ways to check if a URL is absolute in Python. For sake simplicity we'll just stick to .startwith method of a string 
-        # https://stackoverflow.com/questions/8357098/how-can-i-check-if-a-url-is-absolute-using-python 
+
         if not url.startswith('http'):
             url = "https://reddit.com"+url 
-        # You can join urls better using urlparse library of python. 
-        # https://docs.python.org/3/library/urllib.parse.html#urllib.parse.urljoin
+
         print('Extracting data from %s'%url)
-        #Lets wait for 5 seconds before we make requests, so that we don't get blocked while scraping. 
-        #if you see errors that say HTTPError: HTTP Error 429: Too Many Requests , increase this value by 1 second, till you get the data. 
+
         extracted_records.append(parse_comment_page(url))
-        #Lets wait for 10 seconds before we ma 
         # record = {
         #     'title':title,
         #     'url':url
